afe5c6897e8bc8fac05cfb09fe54656d3bbf46cb
Andrew Warren Bailey <andrew@awbconsult.com>
2016-03-04 17:52:36 -0500

    Initial app skeleton and package info

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var express = require("express");
+var express = require('express');
+var compression = require('compression');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(express.static('./public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(cookieParser());
+app.use(compression());
+
+app.get('/', function(req,res) {
+	res.sendfile('./public/vote.html');
+});
+
+app.get('/results', function(req,res) {
+	res.sendfile('./public/results.html');
+});
+
+var server = app.listen(85, function() {
+	console.log('Listening on port %d', server.address().port);
+});
