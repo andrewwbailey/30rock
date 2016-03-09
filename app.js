ad6a754a8e7acb6abd3bcd262be211ec035762b0
Andrew Warren Bailey <andrew@awbconsult.com>
2016-03-09 01:09:36 -0500

    Change to .gitignore and addition of mongoDB hook.

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
+var awbSecure = require('./secure-info.json');
 
 var app = express();
 
+// MongoDB Server connect.
+var dbConnString = "mongodb://" + awbSecure["30rock"]["db"].username + ":" + awbSecure["30rock"]["db"].password + "@" + awbSecure["30rock"]["db"].connloc;
+
+mongoose.connect(dbConnString);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+	console.log("Connected to mLab");
+});
+
+// Express config
 app.use(express.static('./public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'false'}));
@@ -21,6 +36,6 @@ app.get('/results', function(req,res) {
 	res.sendfile('./public/results.html');
 });
 
-var server = app.listen(80, function() {
+var server = app.listen(81, function() {
 	console.log('Listening on port %d', server.address().port);
 });
